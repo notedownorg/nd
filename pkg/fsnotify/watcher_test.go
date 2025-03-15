55812f7982658edc3f94d5c81b266264bfca439d
Liam White <liamawhite@gmail.com>
2025-03-15 11:54:42 -0700

    Add workspace reader (#4)

--- pkg/fsnotify/watcher_test.go
+++ pkg/fsnotify/watcher_test.go
@@ -20,6 +20,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -32,29 +33,41 @@ func randomFile(root string) string {
 	var b strings.Builder
 	b.WriteString(root)
 	b.WriteString("/")
-	for range rand.Intn(5) {
+
+	// Increase chances of top-level directories being unique as there are issues with case sensitive conflicts on some filesystems
+	for range rand.Intn(3) {
+		b.WriteString(words.Random())
+		b.WriteString("-")
+	}
+	b.WriteString(words.Random())
+	b.WriteString("/")
+
+	// Random depth
+	for range rand.Intn(4) {
 		b.WriteString(words.Random())
 		b.WriteString("/")
 	}
+
+	// Random file name
 	b.WriteString(words.Random())
 	b.WriteString(".file")
 	return b.String()
 }
 
 func TestRecursiveWatcher(t *testing.T) {
-	// slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug.Level()})))
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug.Level()})))
 
 	dir, _ := os.MkdirTemp("", "testrecursivewatcher")
 	w, _ := fsnotify.NewRecursiveWatcher(dir)
 
 	// What we want to test is that we get an accurate view of the filesystem based on the events we receive
 	// This because events are non-deteministic even if you dont take ordering into account
-	got := make(fileview)
+	got := &fileview{files: make(map[string]string)}
 	go tracker(t, w, got)
 
 	// Do a bunch of things
-	want := make(fileview)
-	for range 10000 {
+	want := &fileview{files: make(map[string]string)}
+	for range 1000 {
 		path := randomFile(dir)
 		if err := os.MkdirAll(filepath.Dir(path), 0777); err != nil {
 			t.Fatal(err)
@@ -66,12 +79,13 @@ func TestRecursiveWatcher(t *testing.T) {
 			t.Fatal(err)
 		}
 		want.add(path)
-
+	}
+	for path := range want.files {
 		// Randomly update, rename or remove
 		switch rand.Intn(3) {
 		case 0: // Update
 			slog.Debug("updating file", "path", path)
-			content = words.Random()
+			content := words.Random()
 			if err := os.WriteFile(path, []byte(content), 0644); err != nil {
 				t.Fatal(err)
 			}
@@ -86,33 +100,49 @@ func TestRecursiveWatcher(t *testing.T) {
 				t.Fatal(err)
 			}
 			want.add(newpath)
-			delete(want, path)
+			want.delete(path)
 		case 2: // Remove
 			slog.Debug("removing file", "path", path)
 			if err := os.Remove(path); err != nil {
 				t.Fatal(err)
 			}
-			delete(want, path)
+			want.delete(path)
 		}
 	}
 
-	// Wait for the tracker to catch up then compare the views
-	time.Sleep(3 * time.Second)
-	assert.Equal(t, want, got)
+	// Wait for the tracker to catch up
+	// Theres not a good way to do this deterministically
+	time.Sleep(time.Second * 5)
+	defer want.mutex.RUnlock()
+	defer got.mutex.RUnlock()
+	want.mutex.RLock()
+	got.mutex.RLock()
+	assert.Equal(t, want.files, got.files)
 }
 
 // Map of file paths to their content
-type fileview map[string]string
+type fileview struct {
+	mutex sync.RWMutex
+	files map[string]string
+}
 
 func (f *fileview) add(path string) {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		return
 	}
-	(*f)[path] = string(data)
+	f.mutex.Lock()
+	(*f).files[path] = string(data)
+	f.mutex.Unlock()
 }
 
-func tracker(t *testing.T, w *fsnotify.RecursiveWatcher, view fileview) {
+func (f *fileview) delete(path string) {
+	f.mutex.Lock()
+	delete((*f).files, path)
+	f.mutex.Unlock()
+}
+
+func tracker(t *testing.T, w *fsnotify.RecursiveWatcher, view *fileview) {
 	for {
 		select {
 		case event := <-w.Events():
@@ -120,7 +150,7 @@ func tracker(t *testing.T, w *fsnotify.RecursiveWatcher, view fileview) {
 				view.add(event.Name)
 			}
 			if event.Op.Has(fsnotify.Remove) || event.Op.Has(fsnotify.Rename) {
-				delete(view, event.Name)
+				view.delete(event.Name)
 			}
 			if event.Op.Has(fsnotify.Write) {
 				view.add(event.Name)
